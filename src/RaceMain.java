467584123c118c8017645dba8248e35b23ff174c
Vitas1968 <dor1968@mail.ru>
2019-09-04 20:53:21 +0700

    реализация

--- src/RaceMain.java
+++ src/RaceMain.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 public class RaceMain
 {
@@ -13,9 +10,9 @@ public class RaceMain
         //используем  Semaphore для доступа половины участников в тоннель
         Semaphore sem=new Semaphore(CARS_COUNT/2,true);
         // запускаем потоки через ExecutorService
-        ExecutorService executorService= Executors.newFixedThreadPool(4);
+        ExecutorService executorService= Executors.newFixedThreadPool(CARS_COUNT);
         // используем CyclicBarrier для синхронизации потоков на старте и финише
-        CyclicBarrier start=new CyclicBarrier(CARS_COUNT);
+        CyclicBarrier start=new CyclicBarrier(CARS_COUNT+1);
         CyclicBarrier finish=new CyclicBarrier(CARS_COUNT+1);
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
@@ -23,15 +20,35 @@ public class RaceMain
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++)
         {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),sem);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),sem,start,finish);
             executorService.execute( cars[i]);
         }
         executorService.shutdown();
+        try
+        {
+            start.await();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e)
+        {
+            e.printStackTrace();
+        }
 //        for (int i = 0; i < cars.length; i++)
 //        {
 //            new Thread(cars[i]).start();
 //        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try
+        {
+            finish.await();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e)
+        {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
@@ -42,6 +59,8 @@ public class RaceMain
     }
     // добавляем ссылку на семафор в поток
     private Semaphore semaphore;
+     private CyclicBarrier start;
+     private CyclicBarrier finish;
     private Race race;
     private int speed;
     private String name;
@@ -51,10 +70,12 @@ public class RaceMain
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed, Semaphore semaphore) {
+    public Car(Race race, int speed, Semaphore semaphore,CyclicBarrier start,CyclicBarrier finish) {
         this.race = race;
         this.speed = speed;
         this.semaphore=semaphore;
+        this.start=start;
+        this.finish=finish;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -68,22 +89,47 @@ public class RaceMain
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
+        try
+        {
+            start.await();
+            System.out.println(name + " стартанул!");
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e)
+        {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++)
+        {
             if (race.getStages().get(i) instanceof Tunnel)
             { try
                 {
                     System.out.println(name + " ожидает разрешение на въезд в туннель");
                     semaphore.acquire(); // запрос разрешения у семафора
+                    race.getStages().get(i).go(this);
                 } catch (InterruptedException e)
                 {
                     e.printStackTrace();
-                }
-                System.out.println(name+" въезжает в туннель");
-                race.getStages().get(i).go(this);
+                } finally
+            {
+                semaphore.release();
             }
+
+            } else race.getStages().get(i).go(this);
         }
-        System.out.println(name + " освобождает разрешение на въезд в туннель");
-        semaphore.release();
+
+        try
+        {
+            System.out.println(name + " завершил дистанцию");
+            finish.await();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
  abstract class Stage {
